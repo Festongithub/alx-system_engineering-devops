8cc7f6f733d5cf5f092775991cada7971aefbe9a
Festongithub <fkemboik@gmail.com>
2024-06-04 08:38:00 -0700

    Employee data

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,45 +1,53 @@
 #!/usr/bin/python3
-
+"""script that fetches info about a given employee using an api
+and exports it in csv format
 """
-Script that for a given employee ID,
-returns information about his/her TODO list progress.
-"""
-
 import json
 import requests
 import sys
 
 
-url = 'https://jsonplaceholder.typicode.com'
+base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
-    userId = sys.argv[1]
 
-    users_uri = '{}/users?id={}'.format(url, userId)
+    user_id = sys.argv[1]
 
-    response = requests.get(users_uri)
+    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    # print("user url is: {}".format(user_url))
+
+    # get info from api
+    response = requests.get(user_url)
+    # pull data from api
     data = response.text
-    # load the data
+    # parse the data into JSON format
     data = json.loads(data)
-    # extract name
-    name = data[0].get('username')
+    # extract user data, in this case, username of employee
+    user_name = data[0].get('username')
+    # print("id is: {}".format(user_id))
+    # print("name is: {}".format(user_name))
 
-    # Task for the employees
-    task_url = '{}/todos?userId={}'.format(url, userId)
+    # get user info about todo tasks
+    # e.g https://jsonplaceholder.typicode.com/users/1/todos
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    # print("tasks url is: {}".format(tasks_url))
 
-    response = requests.get(task_url)
+    # get info from api
+    response = requests.get(tasks_url)
+    # pull data from api
     tasks = response.text
+    # parse the data into JSON format
     tasks = json.loads(tasks)
 
-    # build the csv data docs
-
-    docs = ""
+    # build the csv
+    builder = ""
     for task in tasks:
-        docs += '"{}", "{}", "{}", "{}"\n'.format(
-                userId,
-                name,
-                task['completed'],
-                task['title']
-                )
-    with open('{}.csv'.format(userId), 'w', encoding='UTF8') as user_file:
-        user_file.write(docs)
+        builder += '"{}","{}","{}","{}"\n'.format(
+            user_id,
+            user_name,
+            task['completed'],  # or use get method
+            task['title']
+        )
+    with open('{}.csv'.format(user_id), 'w', encoding='UTF8') as myFile:
+        myFile.write(builder)
